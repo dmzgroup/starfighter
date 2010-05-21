0c694024056e5e01149808b6ba5bd97bbaded7d7
Randall E. Barker <simstorm@mac.com>
2010-05-21 16:58:56 -0700

    Updated networking configs so transmition rates are lower.

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 . ../scripts/envsetup.sh
 
-$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/audio.xml config/input.xml config/controller.xml config/net.xml config/render.xml config/starfighter.xml config/viper_overlay.xml config/viper_dradis.xml config/viper_hud.xml config/js.xml $*
+$RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/audio.xml config/input.xml config/controller.xml config/render.xml config/starfighter.xml config/net.xml config/viper_overlay.xml config/viper_dradis.xml config/viper_hud.xml config/js.xml $*
