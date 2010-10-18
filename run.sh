8b8db4643e20867e2686dab47ca1be41e89ddb7a
Randall E. Barker <simstorm@mac.com>
2010-10-18 16:13:59 -0700

    Added app names

--- run.sh
+++ run.sh
@@ -2,4 +2,6 @@
 
 . ../scripts/envsetup.sh
 
+export DMZ_APP_NAME=starfighter
+
 $RUN_DEBUG$BIN_HOME/dmzAppQt -f config/runtime.xml config/resource.xml config/common.xml config/audio.xml config/input.xml config/controller.xml config/render.xml config/starfighter.xml config/net.xml config/viper_overlay.xml config/viper_dradis.xml config/viper_hud.xml config/js.xml $*
